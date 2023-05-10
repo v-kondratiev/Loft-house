628010f887ef3b233de5f0af4ce8d03a39ec17d4
v-kondratiev <viktor.kondratiev@gmail.com>
2023-05-10 11:11:18 +0100

    add Benefits add props in Header refactoring Header

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+import logo from "../assets/header/logo.svg";
+
+let data = {
+    header: {
+        links: [
+            {id: 1, name: "About"},
+            {id: 2, name: "Region"},
+            {id: 3, name: "Catalogue"},
+            {id: 4, name: "Contacts"},
+            {id: 5, name: "Payments"},
+        ],
+        logo: logo
+
+    }
+}
+
+export default data;
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
+import data from "./data/data";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+root.render(<App data={data} />);
 
 
